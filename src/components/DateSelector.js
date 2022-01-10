5cf095fb808f771cf056602bbbe4ce3ac8730251
hughsylinden <kaarelinden@hotmail.com>
2022-01-10 23:22:32 +0100

    created new dateselector component

--- src/components/DateSelector.js
+++ src/components/DateSelector.js
@@ -1,23 +1,40 @@
 /* eslint-disable react/no-array-index-key */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import moment from "moment";
+import { getMealsByYear } from "../utils/Meal";
 
-function DateSelector({ selectedMonth, setSelectedDay, setSelectedMonth }) {
-  const months = [
-    "Janaury",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+const months = [
+  "Janaury",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+function DateSelector({ id, selectedMonth, setSelectedDay, setSelectedMonth }) {
+  const [monthsContainingActivity, setMonthsContainingActivity] = useState([]);
+  useEffect(async () => {
+    const formattedDate = moment(
+      `${new Date().getFullYear()}-${selectedMonth}`
+    ).format("YYYY MM DD");
+    await getMealsByYear(id, formattedDate).then((res) => {
+      setMonthsContainingActivity(res.map((day) => new Date(day.time)));
+    });
+  }, [selectedMonth]);
+
+  console.log(
+    monthsContainingActivity
+      .filter((m) => m.getMonth() === months.indexOf(selectedMonth))
+      .map((d) => d.getDate())
+  );
   const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   const dayArray = [...Array(days[months.indexOf(selectedMonth)]).keys()].map(
@@ -25,7 +42,7 @@ function DateSelector({ selectedMonth, setSelectedDay, setSelectedMonth }) {
   );
 
   async function handleSelectMonth(e) {
-    setSelectedMonth(e.target.textContent);
+    setSelectedMonth(e.target.getAttribute("name"));
   }
 
   async function handleSelectDay(e) {
@@ -40,11 +57,15 @@ function DateSelector({ selectedMonth, setSelectedDay, setSelectedMonth }) {
           {months.map((month, i) => (
             <div
               key={i}
+              name={month}
               className="meal-calendar-month"
               onClick={handleSelectMonth}
               aria-hidden="true"
             >
               {month}
+              {monthsContainingActivity
+                .map((m) => m.getMonth())
+                .includes(i) && <>-x</>}
             </div>
           ))}
         </div>
@@ -58,6 +79,10 @@ function DateSelector({ selectedMonth, setSelectedDay, setSelectedMonth }) {
               aria-hidden="true"
             >
               {day}
+              {monthsContainingActivity
+                .filter((m) => m.getMonth() === months.indexOf(selectedMonth))
+                .map((d) => d.getDate())
+                .includes(day) && <>-x</>}
             </div>
           ))}
         </div>
@@ -70,6 +95,7 @@ DateSelector.propTypes = {
   selectedMonth: PropTypes.string.isRequired,
   setSelectedDay: PropTypes.func.isRequired,
   setSelectedMonth: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default DateSelector;
